6c48b6d2bc1d154758652be9b8e642c2c348dc0f
Trustet <yannik@dhcp-vl203-7-142.hs-bremen.de>
2017-05-08 15:08:38 +0200

    Spieler importiert

--- script/core.js
+++ script/core.js
@@ -8,7 +8,8 @@ function init() {
 
 
 }
-
+var cursors;
+var player1;
 var MainGame = function() {
 	
 }
@@ -16,23 +17,32 @@ var MainGame = function() {
 
 
 MainGame.prototype = {
-	init: function(argument){
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-	},
 	
 	preload: function() {
-		
+		game.load.spritesheet('player','assets/dude.png',32,48);
 		
 	},
 	
 	
 	create: function(){
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		player1 = game.add.sprite(32,game.world.height -150,'player');
+		game.physics.arcade.enable(player1);
+		player1.body.collideWorldBounds = true;
+		player1.body.gravity.y = 250;
+		player1.animations.add('left',[0,1,2,3],10,true);
+		player1.animations.add('right'[5,6,7,8],10,true);
 		
-		
+		cursors = game.input.keyboard.createCursorKeys();
 	},
 	
 	update: function() {
+		player1.body.velocity.x = 0;
 		
+		if(cursors.left.isDown){
+			player.body.velocity.x = -150;
+			
+		}
 		
 	}
 
