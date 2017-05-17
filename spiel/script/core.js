4ce3397e4d622736902fad4b72921a448a30c049
Fabian Niehaus <fabian.niehaus.94@gmail.com>
2017-05-17 22:25:17 +0200

    Geht noch nicht

--- spiel/script/core.js
+++ spiel/script/core.js
@@ -8,37 +8,41 @@ function init() {
 
 }
 
-var cursors;
+//var cursors;
 var player1;
 var player2;
 var platforms;
-/*
-var leftBtn;
-var rightBtn;
-var upBtn;
-*/
+
 var MainGame = function() {
 
 };
 
 function MeleeWeapon(x, y, sprite, damage, knockbackAmount, knockbackDirection, game){
     this.object = game.add.sprite(x,y,sprite);
+    this.object.frame = 1;
     game.physics.arcade.enable(this.object);
     this.object.body.collideWorldBounds = true;
     this.damage = damage;
     this.knockbackAmount = knockbackAmount;
     this.knockbackDamage = knockbackDirection;
+
+    this.object.animations.add("meleeAttack",[1,0,1,2,1],9,false);
 }
 
-function Player(x,y,sprite,animations,game) {
+function Player(id,x,y,sprite,animations,game) {
+    this.id = id;
     this.object = game.add.sprite(x,y,sprite);
     game.physics.arcade.enable(this.object);
     this.object.body.collideWorldBounds = true;
     this.object.body.gravity.y = 450;
     var self = this;
+
+    this.meleeTimeout = 0;
+    this.meleeState = 0;
+
     animations.forEach(function(value){
         console.log(value);
-        self.object.animations.add(value);
+        self.object.animations.add(value[0],value[1],value[2],value[3]);
     });
     this.object.direction = 'right';
 
@@ -50,18 +54,33 @@ setMeleeWeapon = function(player, melee){
     player.object.addChild(melee.object);
 };
 
-var projectiles_player1;
-var projectiles_player2;
+/*
+meleeAttack = function(player){
+    player.melee.object.x = player.melee.object.x + 20;
+    player.melee.object.y = player.melee.object.y + 5;
 
-var projectile_timeout_player1 = 0;
-var projectile_timeout_player2 = 0;
+    if (player.meleeTimeout === 0) {
+        if (player.id === 1) {
+            game.physics.arcade.overlap(player.melee.object, player2.object);
+        }
+        if (player.id === 2) {
+            game.physics.arcade.collide(player.melee.object, player1.object);
+        }
+        player.meleeTimeout = 10;
+    } else {
+        player.meleeTimeout--;
+    }
 
-
+    player.melee.object.x = player.melee.object.x - 20;
+    player.melee.object.y = player.melee.object.y - 5;
+};
+*/
 
 MainGame.prototype = {
 
     preload: function() {
         game.load.spritesheet('player','assets/dude.png',32,48);
+        game.load.spritesheet('meleeAttack','assets/meleeAttack.png',60,71);
         game.load.spritesheet('projectile','assets/firstaid.png');
         game.load.spritesheet('player2','assets/baddie.png',32,32);
         game.load.image('ground','assets/platform.png');
@@ -71,23 +90,17 @@ MainGame.prototype = {
     create: function(){
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        animations_player1=[['left',[0,1,2,3],10,true],['right',[5,6,7,8],10,true]];
-        player1 = new Player(160,game.world.height -150,'player',animations_player1,game);
-        //melee1 = new MeleeWeapon(0,0,'projectile',0,0,null,game);
-        //setMeleeWeapon(player1, melee1);
+        var animations_player1=[["left",[0,1,2,3],10,true],["right",[5,6,7,8],10,true]];
+        player1 = new Player(1,160,game.world.height -150,'player',animations_player1,game);
+        melee1 = new MeleeWeapon(-14,-25,'meleeAttack',0,0,null,game);
+        setMeleeWeapon(player1, melee1);
 
-        animations_player2=[['left',[0,1],5,true],['right',[2,3],5,true]];
-        player2 = new Player(708,game.world.height -150,'player2',animations_player2,game);
-        //melee2 = new MeleeWeapon(0,0,'projectile',0,0,null,game);
-        //setMeleeWeapon(player2, melee2);
+        var animations_player2=[["left",[0,1],5,true],["right",[2,3],5,true]];
+        player2 = new Player(2,708,game.world.height -150,'player2',animations_player2,game);
+        melee2 = new MeleeWeapon(0,0,'meleeAttack',0,0,null,game);
+        setMeleeWeapon(player2, melee2);
 
         cursors = game.input.keyboard.createCursorKeys();
-
-        projectiles_player1 = game.add.group();
-        projectiles_player1.enableBody = true;
-
-        projectiles_player2 = game.add.group();
-        projectiles_player2.enableBody = true;
 
         platforms = game.add.group();
         platforms.enableBody = true;
@@ -109,6 +122,7 @@ MainGame.prototype = {
         p2leftBtn = game.input.keyboard.addKey(Phaser.Keyboard.A);
         p2rightBtn = game.input.keyboard.addKey(Phaser.Keyboard.D);
         p2upBtn = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        p2downBtn = game.input.keyboard.addKey(Phaser.Keyboard.S);
 		p2shoot = game.input.keyboard.addKey(Phaser.Keyboard.F);
 		
     },
@@ -117,19 +131,11 @@ MainGame.prototype = {
         game.physics.arcade.collide(player1.object,platforms);
         game.physics.arcade.collide(player2.object,platforms);
         game.physics.arcade.collide(player1.object,player2.object);
-        game.physics.arcade.collide(projectiles_player1,platforms);
-        game.physics.arcade.collide(projectiles_player2,platforms);
-        game.physics.arcade.collide(projectiles_player1,player2.object);
-        game.physics.arcade.collide(projectiles_player2,player1.object);
-        game.physics.arcade.collide(projectiles_player1,projectiles_player2);
-        game.physics.arcade.collide(projectiles_player2,projectiles_player1);
-        //game.physics.arcade.collide(player1.melee.object,player2.object);
+        game.physics.arcade.overlap(player1.melee.object,player2.object,console.log("x"));
 
         player1.object.body.velocity.x = 0;
         player2.object.body.velocity.x = 0;
 
-        if (projectile_timeout_player1 > 0) projectile_timeout_player1--;
-        if (projectile_timeout_player2 > 0) projectile_timeout_player2--;
 
         if(p1cursors.left.isDown){
             player1.object.body.velocity.x = -150;
@@ -145,15 +151,13 @@ MainGame.prototype = {
             player1.object.frame = 4;
         }
 
-        if(p1shoot.isDown && projectile_timeout_player1 === 0){
-            Projectile(player1.object.x,player1.object.y,player1.object.body.velocity.x,1);
-            projectile_timeout_player1 = 10;
+        if(p1cursors.up.isDown && player1.object.body.touching.down){
+            player1.object.body.velocity.y = -350;
         }
 
-		
-		 if(p1cursors.up.isDown && player1.object.body.touching.down){
-		 player1.object.body.velocity.y = -350;
-		 }
+        if(p1cursors.down.isDown){
+            player1.melee.object.animations.play("meleeAttack");
+        }
 		 
 
         if(p2leftBtn.isDown){
@@ -173,66 +177,12 @@ MainGame.prototype = {
 		 if(p2upBtn.isDown && player2.object.body.touching.down){
 		 player2.object.body.velocity.y = -350;
 		 }
-		 
 
-        if(p2shoot.isDown && projectile_timeout_player2 === 0){
-            Projectile(player2.object.x,player2.object.y,player2.object.body.velocity.x,2);
-            projectile_timeout_player2 = 10;
+        if(p2downBtn.isDown){
+            player2.melee.object.animations.play("meleeAttack");
         }
-
-
     }
 
 
 };
 
-function Projectile(x, y, x_velocity, player) {
-
-    var proj = null;
-
-    //Projektil zur Gruppe der Projektile des entsprechenden Spieler hinzufügen
-    switch (player) {
-        case 1:
-
-            proj = projectiles_player1.create(x, y, 'projectile');
-
-            proj_init_vel = 500;
-
-            //Startverktor für Projektil bestimmen
-            if(player1.object.direction === 'right'){
-                proj.body.velocity.x = proj_init_vel + x_velocity;
-            } else {
-                proj.body.velocity.x = -proj_init_vel + x_velocity;
-            }
-
-            break;
-
-        case 2:
-
-            proj = projectiles_player2.create(x,y,'projectile');
-			
-			proj_init_vel = 500;
-            //Startverktor für Projektil bestimmen
-            if(player2.object.direction === 'right'){
-                proj.body.velocity.x = proj_init_vel + x_velocity;
-            } else {
-                proj.body.velocity.x = -proj_init_vel + x_velocity;
-            }
-
-            break;
-
-        default:
-
-            break;
-    }
-
-    //Physik für Projektil aktivieren
-
-
-    //Schwerkraft für Projektil bestimmen
-    proj.body.gravity.y = 50;
-
-    //Bounce für Projektil erstellen
-    proj.body.bounce = 0;
-
-}
